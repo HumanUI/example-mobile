f34f4c6e391bd1d80520edd1564e058fcfacf59b
ChaselWu <minowu@foxmail.com>
2017-02-08 15:07:53 +0800

    Add vue-human alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,7 +25,8 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'vue-human': path.resolve(__dirname, '../vue-human')
     }
   },
   resolveLoader: {
