b7a8f8a171cb1cdb5f6f67df51dffcb45279f0aa
ChaselWu <minowu@foxmail.com>
2017-02-22 15:37:43 +0800

    Change router orderby

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+/* !!Load router into the last */
 import Vue from 'vue'
-import router from './router'
-import App from './app'
 import './human'
 import './vuelidate'
+import App from './app'
+import router from './router'
 
 /* eslint-disable no-new */
 new Vue({
