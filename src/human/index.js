29995a2c58da25ae5088bf59d69b19e0224217ed
ChaselWu <minowu@foxmail.com>
2017-02-23 09:51:37 +0800

    Add example: use custom loading-icon

--- src/human/index.js
+++ src/human/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import Human from 'vue-human'
 import totalSuits from 'vue-human/suits/total'
+// import LoadingIcon from 'vue-human/components/loading-icon/loading-icon'
+// import customLoadingIcon from './loading-icon'
 import DocsLayoutPage from '../components/layout/page'
 import './app.scss'
 
+// Use Human
 Vue.use(Human)
 
-Human.addComponent(DocsLayoutPage)
-Human.addSuits(totalSuits)
+// Custom loading icon
+// Vue.use(LoadingIcon, { icon: customLoadingIcon })
+
+// Use Human Suits
+Vue.use(totalSuits)
+
+// For Global Application
+Vue.component(DocsLayoutPage.name, DocsLayoutPage)
